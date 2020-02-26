dac31659df89afb4b9e88c1e90ffa6389e2e0799
jmnmoyano25 <jmnmoyano@hotmail.com>
2020-02-26 17:37:45 +0100

    .

--- ProyectoPruebaGIT/src/ProbandoGIT.java
+++ ProyectoPruebaGIT/src/ProbandoGIT.java
@@ -3,7 +3,8 @@ public class ProbandoGIT {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-System.out.println("hola");
+System.out.println("hola grtt");
+
 	}
 
 }
